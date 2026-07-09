14230952ffee7a041b8e190086695a551bd8180a
Vmateogms <vicentemateogarciamerino@gmail.com>
2026-07-09 16:14:38 +0200

    changed cors configs, so contact form would work properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import node from '@astrojs/node';
 export default defineConfig({
     output: 'server', 
     adapter: node({mode: 'standalone'}),
+    site: 'https://anabaamondeperez.com',
+    security: {
+        allowedDomains: [
+            { hostname: 'anabaamondeperez.com', protocol: 'https' },
+            { hostname: 'www.anabaamondeperez.com', protocol: 'https' }, // solo si usas www
+        ]
+    },
     i18n: {
         locales: ["es", "en"],
         defaultLocale: "en",
